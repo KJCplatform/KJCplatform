87624e98a2a7031a3ed34d008c5cb755ebc1c16b
wangrujiao <kewpie1992@163.com>
2015-10-15 12:10:45 +0800

    upadate document

--- WebRoot/script/document.js
+++ WebRoot/script/document.js
@@ -1,4 +1,15 @@
 //JQuery的入口
+//获取当前网址，如： http://localhost:8080/ems/Pages/Basic/Person.jsp
+var curWwwPath = window.document.location.href;
+//获取主机地址之后的目录，如： /ems/Pages/Basic/Person.jsp
+var pathName = window.document.location.pathname;
+var pos = curWwwPath.indexOf(pathName);
+//获取主机地址，如： http://localhost:8080
+var localhostPath = curWwwPath.substring(0, pos);
+//获取带"/"的项目名，如：/ems
+var projectName = pathName.substring(0, pathName.substr(1).indexOf('/') + 1);
+var basePath = localhostPath + projectName;
+
 $(function() {
 	listDoc();
 	// 日期加上日期控件
@@ -10,11 +21,11 @@ $(function() {
 	});
 	// 给文本框加上验证器
 	$("#wjm").validatebox({
-		required : true
+		required : true,
 		missingMessage : '文件名不能为空'
 	});
 	$("#wjh").validatebox({
-		required : true
+		required : true,
 		missingMessage : '文件号不能为空'
 	});
 	$("#jbnr").validatebox({
@@ -23,6 +34,7 @@ $(function() {
 });
 //加载公文列表
 function listDoc() {
+	var actionPath = basePath + '/system/XzxzgzbAction_list.action';
 	 $('#dg').datagrid({
             title : '公文管理',
             width : 1200,
@@ -33,13 +45,13 @@ function listDoc() {
             pageList : [ 5, 10, 15, 20 ],//可以选择的分页集合
             nowrap : true,//设置为true，当数据长度超出列宽时将会自动截取
             toolbar:"#toolbar",//在添加 增添、删除、修改操作的按钮要用到这个
-            url:'<%=basePath%>/system/XzxzgzbAction_list.action',//url调用Action方法
+            url:actionPath,//url调用Action方法
             loadMsg : '数据装载中......',
             singleSelect:true,//为true时只能选择单行
             //sortName : 'xh',//当数据表格初始化时以哪一列来排序
             //sortOrder : 'desc',//定义排序顺序，可以是'asc'或者'desc'（正序或者倒序）。
             pagination : true,//分页
-            rownumbers : true//行数
+            rownumbers : true,//行数
 			
 			toolbar:[ {// 工具栏
 				text : '添加',
@@ -61,6 +73,13 @@ function listDoc() {
 				}
 			} ],
         });
+}
+//查询
+function doSearch(){
+	$('#dg').datagrid('load',{
+		wjm: $('#filename').val(),
+		wjh: $('#fileId').val()
+	});
 }
 // 显示编辑窗口
 function showEditForm() {
@@ -105,7 +124,7 @@ function addDoc() {
 }
 
 // 编辑按钮的操作
-function editBook() {
+function editDoc() {
 	var doc = $('#dg').datagrid('getSelected');// 得到选中的一行数据
 	// 如果没有选中记录
 	if (doc	== null) {
@@ -119,9 +138,9 @@ function editBook() {
 	$("#wjh").val(doc.wjh);
 	$("#fwjg").val(doc.fwjg);	
 	// 给默认值
-	$("#fwrq").datebox("setValue", book.pubdate.substring(0, 10));
+	$("#fwrq").datebox("setValue", doc.fwrq.substring(0, 10));
 	$("#jbnr").val(doc.jbnr);
-	$("#jzrq").databox('getValue');
+	$("#jzrq").databox("getValue");
 	$("#jbr").val(doc.jbr);
 	$("#cljg").val(doc.cljg);
 	// 显示编辑页面
@@ -130,9 +149,10 @@ function editBook() {
 function dealSave() {
 	// 表单数据序列化成一个字符串用&拼接
 	var params = $("#frmEdit").serialize();
+	var actionPath = basePath + '/system/XzxzgzbAction_add.action';
 	// 得到doc的值，为空串表示添加的值，为空串表示添加
 	if ($("#id").val() == "") {
-		$.post("<%=basePath%>/system/XzxzgzbAction_add.action", params, function(result) {
+		$.post(actionPath, params, function(result) {
 			if (result.operateSuccess) {
 					$('#dg').datagrid('reload');// 重新加载
 					$.messager.alert('添加', '添加成功', 'info');
@@ -142,7 +162,7 @@ function dealSave() {
 		});
 		} else {
 		// 表示更新
-			$.post("<%=basePath%>/system/XzxzgzbAction_update.action", params, function(result) {
+			$.post(actionPath, params, function(result) {
 				if (result.operateSuccess) {
 					$('#dg').datagrid('reload');// 重新加载
 						$.messager.alert('更新', '更新成功', 'info');
@@ -174,121 +194,40 @@ function save(){
 	});
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-		var url;
-		$(function() {
-        $('#dg').datagrid({
-            title : '公文管理',
-            width : 1200,
-            height: 400,
-            //fit: true,
-            pageSize : 5,//默认选择的分页是每页5行数据
-            pageList : [ 5, 10, 15, 20 ],//可以选择的分页集合
-            nowrap : true,//设置为true，当数据长度超出列宽时将会自动截取
-            toolbar:"#toolbar",//在添加 增添、删除、修改操作的按钮要用到这个
-            url:'<%=basePath%>/system/XzxzgzbAction_list.action',//url调用Action方法
-            loadMsg : '数据装载中......',
-            singleSelect:true,//为true时只能选择单行
-            //sortName : 'xh',//当数据表格初始化时以哪一列来排序
-            //sortOrder : 'desc',//定义排序顺序，可以是'asc'或者'desc'（正序或者倒序）。
-            pagination : true,//分页
-            rownumbers : true//行数
-        });
-         
-    });
-		
-		function addDoc(){
-			$('#frmEdit').dialog('open').dialog('setTitle','添加');
-			$('#fm').form('clear');
-	//		url = 'save_user.php';
-		}
-		function editUser(){
-			var row = $('#dg').datagrid('getSelected');
-			if (row){
-				$('#frmEdit').dialog('open').dialog('setTitle','编辑');
-				$('#fm').form('load',row);
-				url = 'update_user.php?id='+row.id;
-			}
-		}
-		function save(){
-			$('#fm').form('submit',{
-				url: url,
-				onSubmit: function(){
-					return $(this).form('validate');
-				},
-				success: function(result){
-					var result = eval('('+result+')');
-					if (result.success){
-						$('#frmEdit').dialog('close');		// close the dialog
-						$('#dg').datagrid('reload');	// reload the user data
-					} else {
-						$.messager.show({
-							title: 'Error',
-							msg: result.msg
-						});
-					}
-				}
-			});
-		}
-
-		function dealSave() {
-		// 表单数据序列化成一个字符串用&拼接
-			var params = $("#frmEdit").serialize();
-			// 得到doc的值，为空串表示添加的值，为空串表示添加
-			if ($("#doc").val() == "") {
-				$.post("addBook.action", params, function(result) {
+// 删除
+function deleteDoc() {
+	var doc = $('#dg').datagrid('getSelected');// 得到选中的一行数据
+	// 如果没有选中记录
+	if (doc == null) {
+		$.messager.alert('删除', '请先选中要删除的文件', 'info');
+		return;
+	}
+	$.messager.confirm('确认', '真的要删除选中的记录吗？', function(r) {
+		if (r) {
+			var actionPath = basePath + '/system/XzxzgzbAction_delete.action?doc.id=';
+			var url = actionPath + doc.id;
+			// 试一下get方法（地址，回调函数）
+			$.get(url, function(result) {
 				if (result.operateSuccess) {
-					$('#dg').datagrid('reload');// 重新加载
-					$.messager.alert('添加', '添加成功', 'info');
+					$.messager.alert('删除', '选中的文件成功删除！', 'info');
+					// 重新加载
+					$("#dg").datagrid('reload');
 				} else {
-					$.messager.alert('添加', '添加失败', 'warning');
+					$.messager.alert('删除', '删除失败！', 'warning');
 				}
 			});
-			} else {
-			// 表示更新
-				$.post("updateBook.action", params, function(result) {
-					if (result.operateSuccess) {
-						$('#dg').datagrid('reload');// 重新加载
-						$.messager.alert('更新', '更新成功', 'info');
-					} else {
-						$.messager.alert('更新', '更新失败', 'warning');
-					}
-				});
-			}
 		}
-		function removeUser(){
-			var row = $('#dg').datagrid('getSelected');
-			if (row){
-				$.messager.confirm('Confirm','Are you sure you want to remove this user?',function(r){
-					if (r){
-						$.post('remove_user.php',{id:row.id},function(result){
-							if (result.success){
-								$('#dg').datagrid('reload');	// reload the user data
-							} else {
-								$.messager.show({	// show error message
-									title: 'Error',
-									msg: result.msg
-								});
-							}
-						},'json');
-					}
-				});
-			}
-		}
-		function doSearch(){
-			$('#dg').datagrid('load',{
-				wjm: $('#filename').val(),
-				wjh: $('#fileId').val()
-			});
-		}
+	});
+}
+
+
+
+
+
+
+
+
+
+
+
+	
