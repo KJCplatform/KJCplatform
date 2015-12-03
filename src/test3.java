712b678efcc9596a2e2c886ac168e6dfb32caea2
liubiyangwhu <349458066@qq.com>
2015-12-03 10:26:19 +0800

    公文映射改text

--- src/test3.java
+++ src/test3.java
@@ -8,7 +8,7 @@ public class test3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		XzxzgzbService xzxzgzbService=(XzxzgzbService) ServiceProvider.getService(XzxzgzbService.SERVICE_NAME);
-		xzxzgzbService.updateXzxzgzb(null);
+		//xzxzgzbService.updateXzxzgzb(null);
 	}
 
 }
